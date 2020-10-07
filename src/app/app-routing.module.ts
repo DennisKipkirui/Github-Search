5afd428fab51ac2d631981542d4e17844af958ba
Dennis <denismutai95@gmail.com>
2020-10-07 22:58:47 +0300

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from  './home/home.component'
+// import { RepoComponent } from './repo/repo.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'profile', component: ProfileComponent},
+  // { path:  'repo', component: RepoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
